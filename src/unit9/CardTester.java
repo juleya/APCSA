770a19291f01062382e0291146b04dece65680dc
Julia Yang <yangjuliap@gmail.com>
2018-02-26 14:29:03 -0800

    el

---
+++ src/unit9/CardTester.java
@@ -0,0 +1,12 @@
+package unit9;
+
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+}
---
+++ src/unit9/DeckTester.java
@@ -0,0 +1,12 @@
+package unit9;
+
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	}
+}
